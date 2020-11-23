ad620dd050f49ad44ec826eaf2701ca426f2ecbe
Alessandro Tola <alessandro.tola@yahoo.it>
2020-11-23 20:45:31 +0100

    Add go to generick link found by name, go to the next page

--- Web_scraping_selenium/form_scraper.py
+++ Web_scraping_selenium/form_scraper.py
@@ -17,14 +17,16 @@ import time
 #tab_name = sys.argv[2] if sys.argv[2] else "main-keyword-field"
 #search_key = sys.argv[3] if sys.argv[3] else "Casa"
 
-web_site = "https://www.subito.it/"
+#web_site = "https://www.subito.it/"
+web_site = 'https://www.subito.it/annunci-italia/vendita/usato/?q=casa&o=8782'
 tab_name = "main-keyword-field"
 search_key = "Casa"
+oper_browser = True
 
-#Setuo browser configuration
+#Setup browser configuration
 def configure_chrome_browser():
     options = Options()
-    options.headless = True
+    options.headless = False if oper_browser else True
     options.add_argument("--window-size=1920,1200")
 
     DRIVER_PATH = '/Users/alessandrotola/chromedriver'
@@ -35,13 +37,15 @@ def configure_chrome_browser():
 
 driver = configure_chrome_browser()
 
+#Given a tag name and search key we can do a research in the serch bar, and print the titles of the items
 def search_item_form(driver):
     search_field = driver.find_element_by_name(tab_name)
     search_field.send_keys(search_key)
     search_field.send_keys(Keys.RETURN);
     search_button = driver.find_element_by_class_name('button-icon-lens')
     search_button.click()
-
+    print("Element found")
+    
     try:
         items = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, "jsx-59941399.items.visible"))
@@ -50,11 +54,36 @@ def search_item_form(driver):
         for element in elements:
             title = element.find_element_by_tag_name('h2')
             print(title.text)
-        print("Element found")
         
-    finally:
+        
+    except:
         print("No elements with this tag")
         driver.quit()
+#search_item_form(driver)
 
-search_item_form(driver)
+def navigate_link_by_text(driver):        
+    try:
+        link = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.LINK_TEXT, "Inserisci annuncio"))
+        )
+        print("Link found")
+        link.click()
+    except:
+        print("No link found")
+        driver.quit()
+            
+#navigate_link_by_text(driver)
+
+def next_page(driver):
+    try:
+        link = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//*[@id='layout']/main/div[2]/div[5]/div[2]/div[3]/a[2]"))
+        )
+        link.click()
+    except:
+        print("No other pages")
+
+
+
+next_page(driver)
 #driver.quit()

12802f22eee9cafaef296d9e62fe835026f682b9
Alessandro Tola <alessandro.tola@yahoo.it>
2020-11-23 14:05:56 +0100

    Refactor fill and serach in a form

--- Web_scraping_selenium/form_scraper.py
+++ Web_scraping_selenium/form_scraper.py
@@ -1,12 +1,25 @@
 #Switch the package for the scraping, in this project I using selenium instead of scrapy
 #Project: go to the github fill the research field with "python" print the title in the new page loaded
 #python form_scraper.py https://github.com q python
-#argv[1] betsite
+#argv[1] wetsite
 #argv[2] tab name -- for instance in the github file is "q"
 #argv[2] search key
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import sys
+import time
+
+#web_site = sys.argv[1] if sys.argv[1] else "https://www.subito.it/"
+#tab_name = sys.argv[2] if sys.argv[2] else "main-keyword-field"
+#search_key = sys.argv[3] if sys.argv[3] else "Casa"
+
+web_site = "https://www.subito.it/"
+tab_name = "main-keyword-field"
+search_key = "Casa"
 
 #Setuo browser configuration
 def configure_chrome_browser():
@@ -14,18 +27,34 @@ def configure_chrome_browser():
     options.headless = True
     options.add_argument("--window-size=1920,1200")
 
-    DRIVER_PATH = './chromedriver'
+    DRIVER_PATH = '/Users/alessandrotola/chromedriver'
     driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
-    driver.get(sys.argv[1])
+    driver.get(web_site)
 
     return driver
 
 driver = configure_chrome_browser()
 
-field = driver.find_element_by_name(sys.argv[2])
-field.send_keys(sys.argv[3])
-field.submit();
-title = driver.find_element_by_xpath("//*[@id='js-pjax-container']/div/div[3]/div/div[1]/div/div[1]/h3")
-print(title.text)
+def search_item_form(driver):
+    search_field = driver.find_element_by_name(tab_name)
+    search_field.send_keys(search_key)
+    search_field.send_keys(Keys.RETURN);
+    search_button = driver.find_element_by_class_name('button-icon-lens')
+    search_button.click()
 
-driver.quit()
+    try:
+        items = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "jsx-59941399.items.visible"))
+        )
+        elements = items.find_elements_by_class_name('jsx-3924372161.items__item')
+        for element in elements:
+            title = element.find_element_by_tag_name('h2')
+            print(title.text)
+        print("Element found")
+        
+    finally:
+        print("No elements with this tag")
+        driver.quit()
+
+search_item_form(driver)
+#driver.quit()
